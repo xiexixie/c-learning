201ae69dc2b179e635bf2dbb24b03a6101088360
xiexixie <xiejingnan937@qq.com>
2023-06-19 22:23:38 +0800

    simple realize array and vector

---
+++ m_s_datastruct.h
@@ -0,0 +1,109 @@
+#pragma once
+#include <iostream>
+
+template <typename T, size_t n>
+class array
+{
+private:
+  T m_data[n];
+
+public:
+  constexpr size_t size() const { return n; }
+  T &operator[](size_t index) { return m_data[index]; }
+  const T &operator[](size_t index) const { return m_data[index]; }
+  T *data() { return m_data; }
+  const T *data() const { return m_data; }
+};
+
+template <typename T>
+class vector
+{
+private:
+  T *m_data = nullptr;
+  size_t m_size = 0;
+  size_t m_capacity = 0; // size of memory block
+
+  // function of allocating memory of vector again
+  void re_alloc(size_t new_capacity)
+  {
+    // 1.allocate new block of memory
+    // T *new_block = new T[new_capacity];
+    T *new_block = (T *)::operator new(new_capacity * sizeof(T));
+
+    // 2.copy or move,small or big
+    // copy
+    if (new_capacity < m_size)
+      m_size = new_capacity;
+    for (size_t i = 0; i < m_size; i++)
+      new_block[i] = std::move(m_data[i]);
+    // 3.delete old memory
+    // delete[] m_data;
+    for (size_t i = 0; i < m_size; i++)
+      m_data[i].~T();
+    ::operator delete(m_data, m_capacity * sizeof(T));
+    m_data = new_block;
+    m_capacity = new_capacity;
+  }
+
+public:
+  vector()
+  {
+    re_alloc(2);
+  }
+  ~vector()
+  {
+    clear();
+    ::operator delete(m_data, m_capacity * sizeof(T));
+    }
+  void push_back(const T &value)
+  {
+    if (m_size >= m_capacity)
+    {
+      re_alloc(m_capacity + m_capacity / 2); // everytime increase half capacity
+    }
+    m_data[m_size] = value;
+    m_size++;
+  }
+  void push_back(const T &&value)
+  {
+    if (m_size >= m_capacity)
+    {
+      re_alloc(m_capacity + m_capacity / 2); // everytime increase half capacity
+    }
+    m_data[m_size] = std::move(value);
+    m_size++;
+  }
+  template <typename... args>
+  void emplcae_back(args &&...Args)
+  {
+    if (m_size >= m_capacity)
+    {
+      re_alloc(m_capacity + m_capacity / 2); // everytime increase half capacity
+    }
+    // m_data[m_size] = T(std::forward<args>(Args)...);
+    new (&m_data[m_size]) T(std::forward<args>(Args)...);
+    m_size++;
+  }
+  void pop_back()
+  {
+    if (m_size > 0)
+    {
+      m_size--;
+      m_data[m_size].~T();
+    }
+  }
+  void clear()
+  {
+    for (size_t i = 0; i < m_size; i++)
+      m_data[i].~T();
+    m_size = 0;
+  }
+
+  T &operator[](size_t index)
+  {
+    return m_data[index];
+  }
+  const T &operator[](size_t index) const { return m_data[index]; }
+
+  size_t size() const { return m_size; }
+};
---
+++ my.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include "m_s_datastruct.h"
+
+class vector3
+{
+private:
+  float x, y, z;
+  int *a;
+
+public:
+  vector3()
+  {
+    a = new int[5];
+  }
+  vector3(float scalar)
+      : x(scalar), y(scalar), z(scalar)
+  {
+    a = new int[5];
+  }
+  vector3(float x, float y, float z)
+      : x(x), y(y), z(z)
+  {
+    a = new int[5];
+  }
+  vector3(const vector3 &other) = delete;
+
+  vector3(const vector3 &&other)
+      : x(other.x), y(other.y), z(other.z)
+  {
+    a = other.a;
+    std::cout << "copy\n";
+  };
+
+  ~vector3()
+  {
+    delete[] a;
+    std::cout << "destory\n";
+  }
+  vector3 &operator=(const vector3 &other) = delete;
+
+  vector3 &operator=(const vector3 &&other)
+  {
+    std::cout << "move1\n";
+    x = other.x;
+    y = other.y;
+    z = other.z;
+    a = other.a;
+    // other.a = nullptr;
+    return *this;
+  }
+  friend std::ostream &operator<<(std::ostream &stream, const vector3 &v);
+};
+
+std::ostream &operator<<(std::ostream &stream, const vector3 &v)
+{
+  stream << v.x << "," << v.y << "," << v.z;
+  return stream;
+}
+
+int main()
+{
+  /*
+    array<int, 5> a;
+
+    for (int i = 0; i < a.size(); i++)
+    {
+      a[i] = i;
+      std::cout << a[i] << std::endl;
+    }
+    std::cout << "===================" << std::endl;
+  */
+  vector<vector3> v;
+
+  v.emplcae_back(1.0f);
+  v.emplcae_back(2, 3, 4);
+  v.emplcae_back(2, 3, 4);
+  v.emplcae_back(2, 3, 4);
+  v.emplcae_back(2, 3, 4);
+  v.emplcae_back(2, 3, 4);
+  v.emplcae_back(2, 3, 4);
+  v.emplcae_back(5, 6, 7);
+  v.pop_back();
+  v.pop_back();
+  v.pop_back();
+  v.pop_back();
+  v.pop_back();
+  v.clear();
+  for (int i = 0; i < v.size(); i++)
+    std::cout << v[i] << std::endl;
+  std::cout << "===================" << std::endl;
+
+  std::cout << v.size() << std::endl;
+}
